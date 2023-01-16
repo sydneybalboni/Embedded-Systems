a529c191d11d13d14901fc9a61828a7c9ec60148
Sydney Balboni <sydneybalboni@outlook.com>
2023-01-15 21:00:10 -0600

    Remove unfinished code

--- C/Src/keypad.c
+++
@@ -1,9 +0,0 @@
-/**
- ******************************************************************************
- * @file    keypad.c
- * @author  Sydney Balboni
- * @version V1.0
- * @brief   Provides functionality for using the keypad on the
- *          Embedded Systems board
- ******
- */
